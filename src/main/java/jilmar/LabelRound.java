df0d6fc17cc7351d485ddfe08156967d0c394174
USER <USER@JILMAR>
2025-08-12 19:49:01 -0500

    Colores Degradados para los Labels

--- src/main/java/jilmar/LabelRound.java
+++ src/main/java/jilmar/LabelRound.java
@@ -1,7 +1,11 @@
 package jilmar;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GradientPaint;
+import java.awt.LinearGradientPaint;
+import java.awt.Paint;
 import java.awt.RenderingHints;
 import java.awt.Shape;
 import java.awt.geom.Area;
@@ -16,43 +20,47 @@ public class LabelRound extends JLabel {
     private int roundBottomLeft = 0;
     private int roundBottomRight = 0;
 
+    // --- Colores de gradiente (opcionales) ---
+    private Color color1 = null;
+    private Color color2 = null;
+    private Color color3 = null;
+
     public LabelRound() {
         setOpaque(false);
     }
 
-    public int getRoundTopLeft() {
-        return roundTopLeft;
-    }
+    // ----------- Getters y setters normales ----------
+    public int getRoundTopLeft() { return roundTopLeft; }
+    public void setRoundTopLeft(int roundTopLeft) { this.roundTopLeft = roundTopLeft; repaint(); }
 
-    public void setRoundTopLeft(int roundTopLeft) {
-        this.roundTopLeft = roundTopLeft;
+    public int getRoundTopRight() { return roundTopRight; }
+    public void setRoundTopRight(int roundTopRight) { this.roundTopRight = roundTopRight; repaint(); }
+
+    public int getRoundBottomLeft() { return roundBottomLeft; }
+    public void setRoundBottomLeft(int roundBottomLeft) { this.roundBottomLeft = roundBottomLeft; repaint(); }
+
+    public int getRoundBottomRight() { return roundBottomRight; }
+    public void setRoundBottomRight(int roundBottomRight) { this.roundBottomRight = roundBottomRight; repaint(); }
+
+    // ----------- Métodos nuevos para gradiente ----------
+    public void setGradientColors(Color c1, Color c2) {
+        this.color1 = c1;
+        this.color2 = c2;
+        this.color3 = null;
         repaint();
     }
 
-    public int getRoundTopRight() {
-        return roundTopRight;
-    }
-
-    public void setRoundTopRight(int roundTopRight) {
-        this.roundTopRight = roundTopRight;
+    public void setGradientColors(Color c1, Color c2, Color c3) {
+        this.color1 = c1;
+        this.color2 = c2;
+        this.color3 = c3;
         repaint();
     }
 
-    public int getRoundBottomLeft() {
-        return roundBottomLeft;
-    }
-
-    public void setRoundBottomLeft(int roundBottomLeft) {
-        this.roundBottomLeft = roundBottomLeft;
-        repaint();
-    }
-
-    public int getRoundBottomRight() {
-        return roundBottomRight;
-    }
-
-    public void setRoundBottomRight(int roundBottomRight) {
-        this.roundBottomRight = roundBottomRight;
+    public void clearGradient() {
+        this.color1 = null;
+        this.color2 = null;
+        this.color3 = null;
         repaint();
     }
 
@@ -60,22 +68,36 @@ public class LabelRound extends JLabel {
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(getBackground());
+
+        // Crear la forma redondeada combinando esquinas
         Area area = new Area(createRoundTopLeft());
-        if (roundTopRight > 0) {
-            area.intersect(new Area(createRoundTopRight()));
+        if (roundTopRight > 0)  area.intersect(new Area(createRoundTopRight()));
+        if (roundBottomLeft > 0)  area.intersect(new Area(createRoundBottomLeft()));
+        if (roundBottomRight > 0) area.intersect(new Area(createRoundBottomRight()));
+
+        // Pintar fondo sólido o gradiente
+        if (color1 != null && color2 != null) {
+            Paint paint;
+            if (color3 == null) {
+                // Gradiente de 2 colores
+                paint = new GradientPaint(0, 0, color1, 0, getHeight(), color2);
+            } else {
+                // Gradiente de 3 colores
+                float[] fractions = {0f, 0.5f, 1f};
+                Color[] colors = {color1, color2, color3};
+                paint = new LinearGradientPaint(0, 0, 0, getHeight(), fractions, colors);
+            }
+            g2.setPaint(paint);
+        } else {
+            g2.setColor(getBackground()); // comportamiento original
         }
-        if (roundBottomLeft > 0) {
-            area.intersect(new Area(createRoundBottomLeft()));
-        }
-        if (roundBottomRight > 0) {
-            area.intersect(new Area(createRoundBottomRight()));
-        }
+
         g2.fill(area);
         g2.dispose();
         super.paintComponent(g);
     }
 
+    // ----------- Métodos privados para crear esquinas -----------
     private Shape createRoundTopLeft() {
         int width = getWidth();
         int height = getHeight();
